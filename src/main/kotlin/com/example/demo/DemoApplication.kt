470127d48fa768677233fd7a359b3fadc1f367bb
yoooz <oor.9@outlook.com>
2021-12-18 00:29:02 +0900

    delivery web page

--- src/main/kotlin/com/example/demo/DemoApplication.kt
+++ src/main/kotlin/com/example/demo/DemoApplication.kt
@@ -1,5 +1,6 @@
 package com.example.demo
 
+import org.springframework.boot.Banner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
@@ -7,5 +8,7 @@ import org.springframework.boot.runApplication
 class DemoApplication
 
 fun main(args: Array<String>) {
-	runApplication<DemoApplication>(*args)
+	runApplication<DemoApplication>(*args) {
+		setBannerMode(Banner.Mode.OFF)
+	}
 }
