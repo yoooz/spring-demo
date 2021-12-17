8eaa3b696760c258f4b10364b81eed9e37a34f7c
yoooz <oor.9@outlook.com>
2021-12-18 00:46:52 +0900

    create test instance every test time

--- src/test/kotlin/com/example/demo/HtmlControllerTest.kt
+++ src/test/kotlin/com/example/demo/HtmlControllerTest.kt
@@ -1,6 +1,8 @@
 package com.example.demo
 
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.AfterAll
+import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
@@ -10,10 +12,25 @@ import org.springframework.http.HttpStatus
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class HtmlControllerTest(@Autowired val restTemplate: TestRestTemplate) {
+    @BeforeAll
+    fun setup() {
+        println(">> Setup")
+    }
+
     @Test
     fun `Assert page title, content add status code`() {
         val entity = restTemplate.getForEntity<String>("/")
         assertThat(entity.statusCode).isEqualTo(HttpStatus.OK)
         assertThat(entity.body).contains("<h1>Title</h1>")
+    }
+
+    @Test
+    fun `Assert article page title, content add status code`() {
+        println(">> TODO")
+    }
+
+    @AfterAll
+    fun teardown() {
+        println(">> Tear down")
     }
 }
